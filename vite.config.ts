e4292541467f90072b5e601adf0cd211d288a854
Manas Khobrekar <112411630+insert-username-sample@users.noreply.github.com>
2025-02-20 07:16:01 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,9 @@ export default defineConfig({
     assetsDir: 'assets',
     cssCodeSplit: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
