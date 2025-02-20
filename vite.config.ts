47771b40125045db0d127680f3713dc03124f19f
Manas Khobrekar <112411630+insert-username-sample@users.noreply.github.com>
2025-02-20 07:26:14 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -39,3 +39,21 @@ export default defineConfig({
     host: true,
   },
 });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
+})
